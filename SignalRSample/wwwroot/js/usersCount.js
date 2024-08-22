95ffea588bb210348d3be4b152a1bec728c90823
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-22 22:28:14 +0000

    added the script usersCOunt script for updating the userc ount on client

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,32 @@
+// create connection
+
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl('/hubs/userCount').build();
+
+// connect to methods that hub invokes aka receive notifications from hub
+
+connectionUserCount.on('updateTotalViews', (value) => {
+  var newCountSpan = document.getElementById('totalViewsCounter');
+  newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on('updateTotalUsers', (value) => {
+  var newCountSpan = document.getElementById('totalUsersCounter');
+  newCountSpan.innerText = value.toString();
+});
+
+// invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+  connectionUserCount.send('NewWindowReloaded');
+}
+
+// start connection
+
+connectionUserCount
+  .start()
+  .then(function () {
+    console.log('Connection started');
+    newWindowLoadedOnClient();
+  })
+  .catch(function (err) {
+    return console.error(err.toString());
+  });
