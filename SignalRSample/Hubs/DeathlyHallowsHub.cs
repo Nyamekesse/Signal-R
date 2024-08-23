706d5db93d09286a8f03c0e3dd483a9640a60026
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-23 00:02:45 +0000

    added a hub class for deathly hallow

---
+++ SignalRSample/Hubs/DeathlyHallowsHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs;
+
+public class DeathlyHallowsHub : Hub
+{
+    public Dictionary<string, int> GetRaceStatus()
+    {
+        return SD.DealthyHallowRace;
+    }
+}
