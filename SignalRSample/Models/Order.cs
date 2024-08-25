1c24a98dcafb89842cfa924ed98b18d3a825f9ae
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-25 14:37:22 +0000

    created a class to hold attributes related to an order

---
+++ SignalRSample/Models/Order.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRSample.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ItemName { get; set; }
+    public int Count { get; set; }
+}
