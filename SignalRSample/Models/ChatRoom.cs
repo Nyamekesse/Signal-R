ec1d3bcc862e5666b6fec6f492e1edf195435e0c
Nyamekesse <nyamekessesamuel@outlook.com>
2024-08-27 09:17:38 +0000

    defined the model structure for chatroom

---
+++ SignalRSample/Models/ChatRoom.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SignalRSample.Models;
+
+public class ChatRoom
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+}
